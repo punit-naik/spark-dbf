715bb4e037753e66f77194bfcc4547855b271c1d
Punit Naik <naik.punit44@gmail.com>
2017-06-20 15:21:42 +0530

    changed build script and pom.xml

--- spark-dbf.sh
+++ spark-dbf.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
 dest=target
-mvn clean install -Dspark.version=1.3.1 assembly:single
+
+echo "Building spark-dbf project..."
+
+mvn clean install -Dspark.version=1.5.2 assembly:single
 $SPARK_HOME/bin/spark-shell --jars ${dest}/spark-dbf-1.0-jar-with-dependencies.jar
